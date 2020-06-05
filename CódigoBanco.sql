674b5f61c12d8b0718bc48562e66290b8c8b8fbd
ArthurSegato <arthursegato@ctemplar.com>
2020-06-05 08:26:33 -0300

    Update CódigoBanco.sql

--- CódigoBanco.sql
+++ CódigoBanco.sql
@@ -1,5 +1,5 @@
 create table Usuario(
-    id_usuario 			integer 			not null,
+    id					integer 			not null,
     nome 				varchar2(32)		not null,
     sobrenome			varchar2(32)		not null,
     cpf 				varchar2(14) 		not null,
@@ -7,16 +7,20 @@ create table Usuario(
     telefone 			varchar2(15),
     email 				varchar2(32),
     genero 				varchar2(10) 		not null,
-    id_endereco 		integer 			not null
+    id_endereco 		integer 			not null,
+
+    primary key(id)
 );
 
 create table Endereco(
-    id_endereco 		integer 		not null,
+    id 					integer 		not null,
     uf 					varchar2(32) 	not null,
     cidade 				varchar2(32) 	not null,
     bairro 				varchar2(32) 	not null,
     rua 				varchar2(32) 	not null,
-    numero_morada 		integer 		not null
+    numero_morada 		integer 		not null,
+
+    primary key(id_endereco)
 );
 
 create table Professor(
@@ -26,13 +30,17 @@ create table Professor(
     horario_disponivel 	timestamp 		not null,
     tipo_aula 			varchar2(32) 	not null,
     nota 				number 			not null,
-    id_usuario 			integer 		not null
+    id_usuario 			integer 		not null,
+
+    primary key(id_usuario)
 );
 
 create table Aluno(
     instituicao 		varchar2(32) 	not null,
     turma 				varchar2(32) 	not null,
     id_usuario 			integer 		not null,
+
+    primary key(id_usuario)
 );
 
 create table Aula(
@@ -43,35 +51,15 @@ create table Aula(
     avaliacao 				number,
     id_disciplina 			integer		not null,
     id_professor_usuario	integer		not null,
-    id_aluno_uduario 		integer		not null
+    id_aluno_usuario 		integer		not null,
+
+    primary key(id_aula)
 );
 
 create table Disciplina(
     id_disciplina 			integer 		not null,
     nome 					varchar2(32) 	not null,
-    conteudo				varchar2(64) 	not null
+    conteudo				varchar2(64) 	not null,
+
+    primary key(id_disciplina)
 );
-
-#Usuario
-alter table Usuario add constraint  pk_usuario  primary key(id_usuario);
-alter table Usuario add constraint  fk_usuario_endereco foreign key(id_usuario) references Endereco(id_endereco);
-
-#Endereco
-alter table Endereco add constraint  pk_endereco  primary key(id_endereco);
-
-#Professor
-alter table Professor add constraint  pk_professor  primary key(id_usuario);
-alter table Professor add constraint  fk_professor_usuario foreign key(id_usuario) references Usuario(id_usuario);
-
-#Aluno
-alter table Aluno add constraint  pk_aluno  primary key(id_usuario);
-alter table Aluno add constraint  fk_aluno_usuario foreign key(id_usuario) references Usuario(id_usuario);
-
-#Aula
-alter table Aula add constraint  pk_aula  primary key(id_aula);
-alter table Aula add constraint  fk_aula_disciplina foreign key(id_usuario) references Disciplina(id_disciplina);
-alter table Aula add constraint  fk_aula_disciplina foreign key(id_usuario) references Professor(id_professor_usuario);
-alter table Aula add constraint  fk_aula_disciplina foreign key(id_usuario) references Aluno(id_aluno_usuario);
-
-#Disciplina
-alter table Disciplina add constraint  pk_disciplina  primary key(id_disciplina);
