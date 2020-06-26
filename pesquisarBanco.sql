84cd6af5b4f5612cead04e93f4e29b06376c82f6
ArthurSegato <arthursegato@ctemplar.com>
2020-06-25 21:06:40 -0300

    Update pesquisarBanco.sql

--- pesquisarBanco.sql
+++ pesquisarBanco.sql
@@ -1,32 +1,29 @@
-#GERAÇÃO DE RELATÓRIO DA TABELA
-select * from Usuarios;
-
-#Todas as possiveis combinações
-#Seleciona as colunas
-select 
-# 'as' é um apelido
-Cliente.nome as Nome_Cliente,
-Cliente.cpf as CPF_Cliente,
-Vendedor.nome as Nome_Vendedor,
-Vendedor.matricula as Matricula_Vendedor,
-Venda.data_venda as Data_Venda,
-Venda.id as Id_Venda
-#Seleciona as tabelas
-from cliente, vendedor, venda
-#Filtra os resultados
-#Seleciona as vendas que realmente aconteceram
-#nesse caso a achave primária do vendedor
-where Vendedor.id = Venda.id_vendedor
-    and Cliente.id = Venda.id_cliente
-;
-
-Qual os estados ondem os usuários moram?
+Todos os usuários e seus respectivos endereços?
 select
-Usuarios.nome as Nome_Usuario,
-Usuarios.sobrenome as Sobrenome_Usuario,
-Enderecos.uf as Estado_Usuario
-from Usuarios, Enderecos
-where Usuarios.id_endereco = Enderecos.id
-Quais aulas ocorreram?
 
-Onde essas aulas ocorreram?
+Usuarios.nome as Nome,
+Usuarios.sobrenome as Sobrenome,
+Enderecos.uf as Estado,
+Enderecos.cidade as Cidade,
+Enderecos.bairro as Bairro,
+Enderecos.rua as Rua,
+Enderecos.numero_morada as Numero
+
+from Usuarios, Enderecos
+
+where Usuarios.id_endereco = Enderecos.id
+
+Quais aulas ocorreram, quando ocorreram quais foram os alunos e quais foram os tutores?
+
+select
+
+Usuarios.nome as Aluno_Nome,
+Aulas.data_aula as Data_Aula,
+Aulas.local_aula as Local_Aula,
+Aulas.horario_aula as Horario_Aula
+
+from Usuarios, Alunos, Aulas
+
+where Usuarios.id = Alunos.id_usuario and Aulas.id_aluno_usuario = Usuarios.id
+
+Quais as instituições onde os alunos mais tem dificuldades em certas matérias(linkar disciplina com instituição)
